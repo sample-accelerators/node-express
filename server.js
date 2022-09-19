231d8ed5f8f4fe987b1cd4714a5f53dba56e4665
Felipe Tovar <f.tovar12@gmail.com>
2022-09-19 16:19:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server !');
 });
 
 app.get('/health', (req, res) => {
